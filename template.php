a807f4944baecc8da17cfda68f3033c3d54137e3
Mark Carver <mark.carver@me.com>
2013-03-19 10:13:18 -0500

    Fixed blocks not floating when span classes added. Added template.php preprocessing to include row-fluid in blockgroup regions.

--- template.php
+++ template.php
@@ -9,6 +9,7 @@ function bootstrap_subtheme_js_alter(&$js) {
   $theme_js_path = drupal_get_path('theme', 'bootstrap_subtheme') . '/bootstrap/js/bootstrap-';
   if (isset($js[$base_js])) {
     unset($js[$base_js]);
+    // Add Bootstrap JS libraries. Comment out the ones you don't want.
     drupal_add_js($theme_js_path . 'affix.js');
     drupal_add_js($theme_js_path . 'alert.js');
     drupal_add_js($theme_js_path . 'button.js');
@@ -22,5 +23,16 @@ function bootstrap_subtheme_js_alter(&$js) {
     drupal_add_js($theme_js_path . 'tab.js');
     drupal_add_js($theme_js_path . 'transition.js');
     drupal_add_js($theme_js_path . 'typeahead.js');
+  }
+}
+
+/**
+ * Implements hook_preprocess_region().
+ * Add additional classes to block group regions.
+ */
+function bootstrap_subtheme_preprocess_region(&$variables) {
+  if (strpos($variables['region'], 'blockgroup-') === 0) {
+    $variables['classes_array'][] = 'row-fluid';
+    $variables['classes_array'][] = 'equal-height-container';
   }
 }
